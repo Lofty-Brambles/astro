6940e786164b0da7b93b34ced85f566e86b40be6
bholmesdev <hey@bholmes.dev>
2022-12-01 11:44:08 -0500

    fix: escape import.meta.env in `body`

--- packages/astro/src/vite-plugin-utils/index.ts
+++ packages/astro/src/vite-plugin-utils/index.ts
@@ -3,6 +3,15 @@ import ancestor from 'common-ancestor-path';
 import type { AstroConfig, MarkdownAstroData } from '../@types/astro';
 import { appendExtension, appendForwardSlash } from '../core/path.js';
 
+/**
+ * Converts the first dot in `import.meta.env` to its Unicode escape sequence,
+ * which prevents Vite from replacing strings like `import.meta.env.SITE`
+ * in our JS representation of modules like Markdown
+ */
+export function escapeViteEnvReferences(code: string) {
+	return code.replace(/import\.meta\.env/g, 'import\\u002Emeta.env');
+}
+
 export function getFileInfo(id: string, config: AstroConfig) {
 	const sitePathname = appendForwardSlash(
 		config.site ? new URL(config.base, config.site).pathname : config.base
